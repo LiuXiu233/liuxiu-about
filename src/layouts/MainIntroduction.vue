55c9ff92da47a092db0b34194a378f3796057494
刘秀 <i@xiu.buzz>
2021-10-22 10:32:34 +0800

    修正

--- src/layouts/MainIntroduction.vue
+++ src/layouts/MainIntroduction.vue
@@ -6,7 +6,7 @@
     <indented-line>email: <string-wrapper><a href="mailto:i@xiu.buzz">i@xiu.buzz</a></string-wrapper></indented-line>
     <indented-line>profession: <string-wrapper>{{ $t('profession') }}</string-wrapper></indented-line>
     <indented-line>blog: <string-wrapper><a href="https://liuxiu233.com">https://liuxiu233.com</a></string-wrapper></indented-line>
-    <indented-line>studio:<string-wrapper><a href="https://srcmax.com>">SrcMax Studio</a></string-wrapper></indented-line>
+    <indented-line>studio:<string-wrapper><a href="https://srcmax.com">SrcMax Studio</a></string-wrapper></indented-line>
     <sub-obj name="links">
       <indented-line v-for="link in links" :key="link.title"><indentation /><string-wrapper><a :href="link.link" @click="onLinkClick(link)" v-if="link.link">{{ $t(link.title) }}</a><a @click="onLinkClick(link)" v-else class="langOpt">{{ $t(link.title) }}</a></string-wrapper></indented-line>
     </sub-obj>
