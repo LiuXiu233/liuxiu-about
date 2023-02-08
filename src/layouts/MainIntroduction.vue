28c853279d7317437c707e9f0d8e1d46c31f34e4
刘秀 <i@xiu.buzz>
2023-02-08 16:40:23 +0800

    Revert "Update MainIntroduction.vue"
    This reverts commit a379529f39ee04a58996e16b8c79affea57a1a19.

--- src/layouts/MainIntroduction.vue
+++ src/layouts/MainIntroduction.vue
@@ -6,7 +6,6 @@
     <indented-line>email: <string-wrapper><a href="mailto:xiu@srcmax.com">Xiu@srcmax.com</a></string-wrapper></indented-line>
     <indented-line>profession: <string-wrapper>{{ $t('profession') }}</string-wrapper></indented-line>
     <sub-obj name="blog">
-      <indented-line><indentation />english: <string-wrapper><a href="https://blog.liuxiu.io">https://blog.liuxiu.io</a></string-wrapper></indented-line>
       <indented-line><indentation />general: <string-wrapper><a href="https://liuxiu233.com">https://liuxiu233.com</a></string-wrapper></indented-line>
       <indented-line><indentation />e-sport: <string-wrapper><a href="https://esport.liuxiu.io">https://esport.liuxiu.io</a></string-wrapper></indented-line>
       <indented-line><indentation />writing: <string-wrapper><a href="https://writing.liuxiu.io">https://writing.liuxiu.io</a></string-wrapper></indented-line>
